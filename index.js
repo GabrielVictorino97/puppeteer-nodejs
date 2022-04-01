cbbcc9c9746e415e8e75edbbafa113df95d0a596
Gabriel Victorino <gavictorino97@gmail.com>
2022-03-31 22:22:28 -0300

    teste commit via zsh

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 const readlineSync = require('readline-sync');
 
-console.log("Conversor de moedas 💰");
+console.log("Conversor de moedas 💰💻");
 
 (async() => {
     const browser = await puppeteer.launch();
